326c559cadd02bb799ac2af84b7adb1538bfea2c
Muteeb Nawaz <mutnawaz@cisco.com>
2022-02-22 23:17:52 +0530

    Initial code

---
+++ README.md
@@ -0,0 +1,46 @@
+# Quic Classifier of QUIC and Non-QUIC packets
+
+## 1. Capture packets
+
+Download and install wireshark and capture packets and save as pcap file.
+
+or
+
+Capture packets using `tcpdump`
+```sh
+tcpdump -s 0 -i <interface-name> -w <file-name.pcap>
+```
+Example:
+```sh
+tcpdump -s 0 -i en0 -w initial-capture.pcap
+```
+
+[tcpdump to pcap](https://linuxexplore.com/2012/06/07/use-tcpdump-to-capture-in-a-pcap-file-wireshark-dump/)
+
+## 2. Convert pcap files to CSV.
+
+There are many ways to convert pcap to csv. But we need only specific feilds and this is easiest way. Command to generate CSV from pcap file.
+
+```sh
+tshark -r initial-capture.pcap  -T fields -E header=y -E separator=, -E occurrence=f  -e frame.encap_type -e frame.time_epoch -e frame.len -e frame.cap_len -e eth.src -e eth.dst -e ip.version -e ip.hdr_len -e ip.tos -e ip.id -e ip.flags -e ip.flags.rb -e ip.flags.df -e ip.flags.mf -e ip.frag_offset -e ip.ttl -e ip.proto -e ip.checksum -e ip.src -e ip.dst -e ip.len -e ip.dsfield -e tcp.srcport -e tcp.dstport -e tcp.hdr_len -e tcp.checksum -e udp.srcport -e udp.dstport -e udp.length -e udp.checksum -e quic > initial.csv
+```
+
+## [TODO] Collect all the csv and shuffle the rows.
+
+## 4. Merge the respective columns from tcp & udp.
+
+As we know QUIC uses UDP protocol. Therefore, we have taken common feilds from TCP and UDP protocol ie, srcport,dstport,length,checksum. Run the below command to merge the columns and rename the column names.
+
+**Before:** tcp.srcport,tcp.dstport,tcp.hdr_len,tcp.checksum,udp.srcport,udp.dstport,udp.length,udp.checksum
+
+**After :** srcport,dstport,length,checksum
+
+```
+python3 merge_columns.py
+```
+
+## 5. Run the ML Models
+
+Run all the cells.
+
+`quic-classifier.ipynb`
---
+++ merge_columns.py
@@ -0,0 +1,114 @@
+# Goal merge the columns
+# Before: tcp.srcport,tcp.dstport,tcp.hdr_len,tcp.checksum,udp.srcport,udp.dstport,udp.length,udp.checksum
+# After : srcport,dstport,length,checksum
+
+import csv
+import pandas as pd
+
+INPUT_FILE = "dataset.csv"
+OUTPUT_FILE = "output-dataset.csv"
+
+quic_counter = 0
+total_packets = 0
+
+with open(INPUT_FILE) as f, open(OUTPUT_FILE, "w") as g:
+    reader = csv.reader(f)
+    writer = csv.writer(g)
+
+    headers = next(reader, None)  # skip the headers
+    writer.writerow(headers[:-4])
+    for row in reader:
+        # print("Merging srcport...")
+        if not row[22] and not row[26]:
+            srcport = ""
+        elif row[22]:
+            srcport = row[22]
+        elif row[26]:
+            srcport = row[26]
+        else:
+            srcport = ""
+
+        if not srcport:
+            print("Empty srcport, skipping row: ", row)
+            continue
+
+        # print("Merging dstport...")
+        if not row[23] and not row[27]:
+            dstport = ""
+        elif row[23]:
+            dstport = row[23]
+        elif row[27]:
+            dstport = row[27]
+        else:
+            dstport = ""
+
+        if not dstport:
+            print("Empty dstport, skipping row: ", row)
+            continue
+
+        # print("Merging length...")
+        if not row[24] and not row[28]:
+            length = ""
+        elif row[24]:
+            length = row[24]
+        elif row[28]:
+            length = row[28]
+        else:
+            length = ""
+
+        if not length:
+            print("Empty length, skipping row: ", row)
+            continue
+
+        # print("Merging checksum...")
+        if not row[25] and not row[29]:
+            checksum = ""
+        elif row[25]:
+            checksum = row[25]
+        elif row[29]:
+            checksum = row[29]
+        else:
+            checksum = ""
+
+        if not checksum:
+            print("Empty checksum, skipping row: ", row)
+            continue
+
+        # print(row[-1])
+        if row[-1]:
+            quic = 1
+            quic_counter += 1
+        else:
+            quic = 0
+
+        new_row = row[:22] + [srcport] + [dstport] + [length] + [checksum] + [quic]
+        # print(new_row)
+
+        total_packets += 1
+        writer.writerow(new_row)
+
+
+print("\nRenaming columns...")
+# Load data from a CSV file into a Pandas df:
+df = pd.read_csv(OUTPUT_FILE)
+
+# rename column names from the CSV file
+df.columns.values[22] = "srcport"
+df.columns.values[23] = "dstport"
+df.columns.values[24] = "length"
+df.columns.values[25] = "checksum"
+df.columns.values[26] = "quic"
+
+
+df.to_csv(OUTPUT_FILE, index=False)
+print("Updated column names: ", df.columns)
+
+
+print(
+    "\nQuic packets: {0}, Non-Quic packets: {1}, Total: {2}, Percentage: {3}%".format(
+        quic_counter,
+        total_packets - quic_counter,
+        total_packets,
+        round((quic_counter / total_packets) * 100, 2),
+    )
+)
